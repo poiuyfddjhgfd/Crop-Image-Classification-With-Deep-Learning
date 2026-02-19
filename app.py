68657aa69aa13f87a61022f59aa7788f764b2982
RAHUL KUMAR YADAV <rahulyadavbitmesra4@gmail.com>
2026-02-19 16:22:24 +0530

    Update and rename streamlit_app.py to app.py

---
+++ app.py
@@ -0,0 +1,42 @@
+import gradio as gr
+import torch
+from torchvision import models, transforms
+from PIL import Image
+
+model_data = torch.load(
+    "crop_classifier_model_fixed.pth",
+    map_location="cpu",
+    weights_only=False
+)
+
+model = models.resnet18(pretrained=False)
+
+model.fc = torch.nn.Linear(
+    model.fc.in_features,
+    len(model_data["class_to_idx"])
+)
+
+model.load_state_dict(model_data["model_state_dict"])
+model.eval()
+
+idx_to_class = {
+    v:k for k,v in model_data["class_to_idx"].items()
+}
+
+transform = transforms.Compose([
+    transforms.Resize((224,224)),
+    transforms.ToTensor()
+])
+
+def predict(image):
+    image = transform(image).unsqueeze(0)
+    output = model(image)
+    _, pred = torch.max(output,1)
+    return idx_to_class[pred.item()]
+
+gr.Interface(
+    fn=predict,
+    inputs=gr.Image(type="pil"),
+    outputs="text",
+    title="Crop Classifier"
+).launch()
